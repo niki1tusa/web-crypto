c61410f069b45f4d0342e7ec89561eee0155b701
Nikonov Nikita <niki2ta00n4@gmail.com>
2025-04-12 22:46:32 +0300

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,22 @@
-import express from 'express'
-import * as trcpExpress from '@trpc/server/adapters/express'
-import { trpcRouter } from './trpc'
-import cors from 'cors'
+import express from "express"
+import * as trcpExpress from "@trpc/server/adapters/express"
+import { trpcRouter } from "./trpc"
+import cors from "cors"
 
 const app = express()
 app.use(cors())
 
-
-app.get('/ping', (req, res)=>{
-    res.send('pong')
+app.get("/ping", (req, res) => {
+  res.send("pong")
 })
 
-app.use('/trpc', trcpExpress.createExpressMiddleware({
-    router: trpcRouter
-}))
+app.use(
+  "/trpc",
+  trcpExpress.createExpressMiddleware({
+    router: trpcRouter,
+  }),
+)
 
-
-app.listen(5433, ()=>{
-    console.info('listening at http://localhost:5433')
+app.listen(5433, () => {
+  console.info("listening at http://localhost:5433")
 })
