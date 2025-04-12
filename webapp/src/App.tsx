9526a52816b190d79555653953571153f52515ee
Nikonov Nikita <niki2ta00n4@gmail.com>
2025-04-12 22:32:55 +0300

    add

---
+++ webapp/src/App.tsx
@@ -0,0 +1,10 @@
+import { TrpcProvider } from "./lib/trpc"
+import { Data } from "./pages/AllideasPage"
+
+export const App = () => {
+  return (
+    <TrpcProvider>
+      <Data />
+    </TrpcProvider>
+  )
+}
